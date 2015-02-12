d6dcfb7ca2c6e014d5b65a4062a93248ee4b6373
SteyfLab <stephane.gouloumes@gmail.com>
2015-02-12 09:22:46 +0100

    Deploy

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ var Room = require('./app/room.js');
 			//-- Configuration --//
 		   //-------------------//
 
-mongoose.connect(db.url);							// Connection à la DB
-var database = mongoose.connection;
-
 var app = express(); // Express
 var port = process.env.PORT || 1337;
 var server = require('http').createServer(app).listen(port);
@@ -29,22 +26,6 @@ app.use(express.static(__dirname + '/public'));		// Set le répertoire public
 app.get('/', function(req, res) {
   res.render('index.html');
 });
-
-			 //-----------------------------------------//
-			//-- Déclaration des variables générales --//
-		   //-----------------------------------------//
-
-var users = {}; 		// Objet users
-var usersCount = 0;
-var rooms = {}; 		// Objet rooms
-var roomsCount = 0;
-var games = [];			// Games
-var gamesCount = 0;
-var playersCount = 0;
-var messages = []; 		// Tableau qui stocke les messages
-var messagesGen = []; 	// Tableau qui stocke les messages du général
-var messagesRooms = []; // Tableau qui stocke les messages pour chaque Room
-var history = 2;
 
 			 //------------------------//
 			//-- Sockets Connection --//
